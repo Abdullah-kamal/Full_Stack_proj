031aaf36ca1280c797b2d66fd5c614ec80d2ec2c
Abdullah-kamal <Abdullahkamal207@gmail.com>
2023-12-06 00:37:35 +0530

    0.1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,34 @@
-const App = (props) => {
-  const {counter} = props
+import React from 'react';
+import { useState } from 'react';
+
+const App = () => {
+  // these are ok
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const LeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+    setTotal(left + right);
+    console.log("i am left: ",left);
+  }
+  const RightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+    setTotal(left + right);
+    console.log("i am right: ",right);
+  }
   return (
-    <div>{counter}</div>
+    <div>
+      <p>{RightClick} and {LeftClick}</p>
+      <p>{total}</p>
+      <button onClick={RightClick}>
+        right click here</button>
+      <button onClick={LeftClick}>
+        left click here</button>
+    </div>
   )
 }
 
