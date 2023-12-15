9b01492be2690fe05d748a192943b6238cbf683f
Abdullah-kamal <Abdullahkamal207@gmail.com>
2023-12-15 22:24:13 +0530

    1.1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,109 @@
 import { useState } from 'react'
 
+const StatisticsLines = (props) => {
+  return (
+      <tbody>
+        <tr>
+          <th>Statistics</th>
+        </tr>
+        <tr>
+          <td>Total:</td>
+          <td>{props.total}</td>
+        </tr>
+        <tr>
+          <td>Good:</td>
+          <td>{props.good}</td>
+        </tr>
+        <tr>
+          <td>Bad:</td>
+          <td>{props.bad}</td> 
+        </tr>
+        <tr>
+          <td>Neutral:</td>
+          <td>{props.neutral}</td>
+        </tr>
+        <tr>
+          <td>Average:</td>
+          <td>{props.average}</td>
+        </tr>
+      </tbody>
+  )
+}
+const Statistics = (props) => {
+  const isStatistic = props.isStatistic;
+  return (
+    <div>
+      {isStatistic ? (
+        <div className='statistics'>
+          <h1> No Feedback Given Yet! </h1>
+        </div>
+      ) : (
+        <table>
+          <StatisticsLines
+            good={props.good}
+            total={props.total}
+            bad={props.bad}
+            average={props.average}
+            neutral={props.neutral} />
+        </table>
+      )}
+
+      <table className='feedbacks'>
+      <tbody>
+        <tr>
+          <th><h3>Give feedbacks</h3></th>
+        </tr>
+        <tr>
+            <td>
+              <button onClick={props.thisGood}> 👍 </button>
+              <button onClick={props.thisBad}> 👎 </button>
+              <button onClick={props.thisNeutral}> 😐 </button>
+              </td> 
+        </tr>
+          </tbody>
+      </table>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0);
+  const all = good + bad + neutral;
+  const average = total / all;
+  const isStatistic = (good + bad + neutral) == 0;
   const thisGood = () => {
-    setGood(good + 1);
+    setGood((curr) => curr + 1);
+    setTotal(total + 1);
   }
   const thisBad = () => {
-    setBad(bad + 1);
+    setBad((curr) => curr + 1);
+    setTotal(total - 1);
   }
   const thisNeutral = () => {
-    setNeutral(neutral + 1);
+    setNeutral((curr) => curr + 1);
+    setTotal(total + 0);
+    console.log(neutral);
   }
+
   return (
     <div>
-      <h3>Give feedbacks</h3>
-      <br></br>
-      <button onClick={thisGood}> 👍 </button>
-      <button onClick={thisBad}> 👎 </button>
-      <button onClick={thisNeutral}> 😐 </button>
-      <h3> Statistics </h3>
-      <p>Good : {good}</p>
-      <p>Bad : {bad}</p>
-      <p>Neutral : {neutral}</p>
+
+      <div>
+        <Statistics
+          isStatistic={isStatistic}
+          good={good}
+          total={total}
+          bad={bad}
+          average={average}
+          neutral={neutral}
+          thisGood={thisGood}
+          thisBad={thisBad}
+          thisNeutral={thisNeutral} />
+      </div>
     </div>
   )
 }
