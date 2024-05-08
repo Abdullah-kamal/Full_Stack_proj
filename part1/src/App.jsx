00923e85b06811b972c986d6d5e6894b40c21283
Abdullah-kamal <Abdullahkamal207@gmail.com>
2024-05-08 13:47:36 +0530

    1.2.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,77 +1,36 @@
-import { useState } from 'react';
-
-
-
-const Square = ({ value, onSquareClick }) => {
-  return <button className="square" onClick={onSquareClick}>{value} </button>;
-}
-
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-  const winner = calculateWinner(squares);
-    let gameStatus;
-    if (winner) {
-      gameStatus = "Winner: " + winner;
-    } else {
-      gameStatus = "Next player is: " + (xIsNext ? 'X' : 'O');
-    }
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquare = squares.slice();
-    if (xIsNext) {
-      nextSquare[i] = 'X';
-    } else {
-      nextSquare[i] = 'O';
-    }
-    
-    setSquares(nextSquare);
-    setXIsNext(!xIsNext);
-    console.log("a")
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ],
   }
+  let name = course.name;
   return (
     <>
-      <div className="status">
-        {gameStatus}
-      </div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
-        <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
-        <Square value={squares[2]} onSquareClick={() => { handleClick(2) }} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => { handleClick(3) }} />
-        <Square value={squares[4]} onSquareClick={() => { handleClick(4) }} />
-        <Square value={squares[5]} onSquareClick={() => { handleClick(5) }} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => { handleClick(6) }} />
-        <Square value={squares[7]} onSquareClick={() => { handleClick(7) }} />
-        <Square value={squares[8]} onSquareClick={() => { handleClick(8) }} />
-      </div>
+      <h1>{name}</h1>
+      {course.parts.map((parts) => 
+        {
+        return <li key={parts.id}>{parts.name} {parts.exercises}</li>
+        })}
     </>
-  );
+  )
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 export default App
